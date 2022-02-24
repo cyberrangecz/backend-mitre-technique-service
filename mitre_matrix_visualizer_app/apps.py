a7ca4be3d689f6c784340bfd0f6892f358369502
Michal Urban <485429@mail.muni.cz>
2022-02-24 15:43:10 +0100

    Initial commit

---
+++ mitre_matrix_visualizer_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MitreMatrixVisualizerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mitre_matrix_visualizer_app'
